e4006a735f5febd780252fdaccbc46e0ae3475ab
Akiko Green <akikogreen08@gmail.com>
2020-03-22 01:31:56 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,28 @@
+require "pry" 
+# put this at top
+# this is your best friend for if you need to know how 
+# your code is doing in the terminal type gem install pry
+# hard exit = exit!,,, soft exit = exit (you can check on how the loop is doing here)
+#binding.pry anywhere to sop and check how your iteration is doing 
+
+def reverse_each_word(string)
+ split_array = string.split(' ')
+ reversed = []
+ split_array.each do |word|
+    reversed << word.reverse
+  end
+return reversed.join(' ')
+end
+
+
+def reverse_each_word(string)
+  original_string = string.split(" ")
+ original_string.collect do |word|
+   new_array = word.reverse
+   new_array.join(" ")
+  end
+
+
+
+
+
